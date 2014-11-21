535c5a84eb8f4728a5257b564e7d236a334deeb7
delsigno <keithpapa116@gmail.com>
2014-11-21 13:09:06 -0500

    Yolo nothing works

--- src/StatDriver.java
+++ src/StatDriver.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 
 import cryptostats.algorithms.AlgAES;
@@ -19,7 +20,8 @@ public class StatDriver {
 	//SIMON and SPECK must have 128/8 file size
 	private static final int PLAINTEXTLENGTH = 128/8;//in bytes
 	private static final long NANOCONVERTER = 1000000000L;
-	private static final double MEANTOL = 20;//in bytes/sec
+	private static final double MEANTOL = .1;//in bytes/sec
+	private static final int ALGCOUNT = 4;
 	
 	
 
@@ -36,7 +38,14 @@ public class StatDriver {
 				new AlgSet(new AlgAES(),"AlgAES"),
 				new AlgSet(new AlgSERPENT(),"AlgSERPENT"),
 				new AlgSet(new AlgSIMON(),"AlgSIMON"),
-				new AlgSet(new AlgSPECK(),"AlgSPECK"),
+				new AlgSet(new AlgSPECK(),"AlgSPECK")
+		};
+		
+		double[] avgEncDecTime = {
+				0, //AES
+				0, //Serpent
+				0, //Simon
+				0  //Speck
 		};
 
 		String dirName = System.getProperty("user.dir");       // Used for code conversion
@@ -50,6 +59,10 @@ public class StatDriver {
 		for(int i = 0; i < 4; i++){
 			meta.update(algorithms[i]);
 			
+			//Resets our timeing for every algorithm, although it shouldnt matter
+			//abpsm = new double[]{0,0,0,0,0};
+			
+			//Gathers data until variation is reduced to a certain tolarance for the last 5 runs
 			while(testing != (-1)){
 				testing++;
 				TestTimer timer = meta.testFileTime(PLAINTEXTLENGTH);
@@ -78,15 +91,22 @@ public class StatDriver {
 						continue;
 					}
 				}	
+				
+				//Assigns our final average value to the specific cipher
+				avgEncDecTime[i] = abpsm[4];
 			}
 			
-				  
+				
+			//Prints out the data for the specific cipher
 		    try {
 		    	File tempFile = new File(dirName + "\\" + "data_" + algorithms[i].getName());
-			    FileOutputStream fos = new FileOutputStream(tempFile);
-			    ObjectOutputStream oos = new ObjectOutputStream(fos); 
-				oos.writeObject(data[i]);
-				oos.close(); 
+			    FileOutputStream fos = new FileOutputStream(tempFile); 
+			    PrintWriter pw = new PrintWriter(tempFile);
+				for(int d = 0; d < data[i].size(); d++){
+					//System.out.println("print writer printing");
+					pw.printf("%.12f\n" , data[i].get(d));
+				}
+				pw.close();
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -102,6 +122,26 @@ public class StatDriver {
 			*/
 		}
 		
+		
+		//Prints out the data for the specific cipher
+	    try {
+	    	File tempFile = new File(dirName + "\\" + "data_global");
+		    FileOutputStream fos = new FileOutputStream(tempFile); 
+		    PrintWriter pw = new PrintWriter(tempFile);
+
+
+		    //prints data per alg
+		    for(int i = 0; i < ALGCOUNT; i++){
+		    	pw.println("-------" + algorithms[i].getName() + "-------");
+		    	
+		    	pw.print("final average encryption time after convergence: ");
+		    	pw.printf("%.12f\n" , avgEncDecTime[i]);
+		    }
+			pw.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
 
 		
 	}
